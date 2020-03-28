e9160bcf7725165e0b9d7ea2297a45c5034c6be9
Ezequiel Tosi <ezequieltosi.96@hotmail.com>
2020-03-28 18:11:09 -0400

    React Project Added

---
+++ client-app/src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// ReactDOM solo tiene un metodo y ese metodo es render()
+// el metodo render tiene dos argumentos.
+// El primero es el componente de nuestro proyecto a renderizar
+// y el segundo es el contenedor donde se renderizara el componente
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
